1939616ed45610a94af4ccd0204d6b6fa2b01f08
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-01-24 21:31:56 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="vizblend",
-    version="2.0.0",
+    version="2.0.1",
     author="Mahmoud Housam",
     author_email="mahmoudhousam60@gmail.com",
-    description="Seamlessly integrates Plotly’s interactive visualizations into a single standalone HTML file offering a modern presentation-like experience with the flexibility of a web-based report",
+    description="Seamlessly integrates Plotly interactive visualizations into a single standalone HTML file offering a modern presentation-like experience with the flexibility of a web-based report",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/MahmoudHousam/VizBlend",
